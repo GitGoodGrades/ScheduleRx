8d7c7432aeb42249329703ff81d8095d68aff46c
WoodNj <Woodnj@warhawks.ulm.edu>
2018-02-22 16:59:27 -0600

    WIP

---
+++ ScheduleRx.Reactjs/schedulerx/src/components/Calendar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import styles from 'react-big-calendar/lib/css/react-big-calendar.css';
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+BigCalendar.momentLocalizer(moment); // or globalizeLocalizer
+
+const Calendar = props => (
+  <div>
+    <BigCalendar
+      events={props.events}
+      startAccessor='startDate'
+      endAccessor='endDate'
+    />
+  </div>
+);
+export default Calendar;
--- ScheduleRx.Reactjs/schedulerx/src/scenes/Home/Home.jsx
+++ ScheduleRx.Reactjs/schedulerx/src/scenes/Home/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import List from 'material-ui/List';
 import { ListItems} from '../../components/tileData';
+import Calendar from '../../components/Calendar';
 
-const Home = () => <div><List><ListItems /></List></div>;
+const eventList = [
+    {
+      id: 0,
+      title: 'All Day Event very long title',
+      allDay: true,
+      start: new Date(2018, 2, 10),
+      end: new Date(2018, 2, 11),
+    },
+    {
+      id: 1,
+      title: 'Long Event',
+      start: new Date(2018, 2, 12),
+      end: new Date(2015, 2, 13),
+    },
+]  
+
+const Home = () => <div height="100%"><Calendar events={eventList}/></div>;
 
 export default Home;
