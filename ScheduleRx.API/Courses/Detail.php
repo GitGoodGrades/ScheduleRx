398f03000512fc893257145819ee071bd97f1bf3
stevensavant <stevensavant@gmail.com>
2018-02-23 01:21:15 -0600

    update

---
+++ ScheduleRx.API/Courses/Detail.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Credentials: true");
+header('Content-Type: application/json');
+
+include_once '../config/database.php';
+include_once '../models/Course.php';
+
+$database = new Database();
+$db = $database->getConnection();
+$data = json_decode(file_get_contents("php://input"));
+
+$query = "SELECT COURSE_ID, STUDENTS
+            FROM course
+            WHERE COURSE_ID = ?
+            LIMIT 0,1";
+
+$stmt = $db->prepare( $query );
+$stmt->bindParam(1,$data->COURSE_ID);
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($row) {
+    print_r(json_encode($row));
+}
+else {
+    echo null;
+}
