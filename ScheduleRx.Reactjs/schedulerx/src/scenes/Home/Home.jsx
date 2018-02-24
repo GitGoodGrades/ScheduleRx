bc6d282700dbfe228a820d26b882c449452f1f04
WoodNj <Woodnj@warhawks.ulm.edu>
2018-02-24 17:48:30 -0600

    Populate calendar from API

--- ScheduleRx.Reactjs/schedulerx/src/scenes/Home/Home.jsx
+++ ScheduleRx.Reactjs/schedulerx/src/scenes/Home/Home.jsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Calendar from '../../components/Calendar';
+import axios from 'axios';
+import moment from 'moment';
 
-const events = [
-    {
-      title: 'Event very long title',
-      startDate: new Date(2018, 1, 10, 20, 0, 0, 0),
-      endDate: new Date(2018, 1, 10, 21, 0, 0, 0),
-    },
-    {
-      title: 'Long Event',
-      startDate: new Date(2018, 1, 18, 20, 0, 0, 0),
-      endDate: new Date(2018, 1, 19, 21, 0, 0, 0),
-    },
-]  
+// const events = [
+//     {
+//       title: 'Event very long title',
+//       startDate: new Date(2018, 1, 10, 20, 0, 0, 0),
+//       endDate: new Date(2018, 1, 10, 21, 0, 0, 0),
+//     },
+//     {
+//       title: 'Long Event',
+//       startDate: new Date(2018, 1, 18, 20, 0, 0, 0),
+//       endDate: new Date(2018, 1, 19, 21, 0, 0, 0),
+//     },
+// ]  
 
-const Home = () => <div minHeight="700" className="rbc-calendar"><Calendar events={events}/></div>;
+class Home extends Component {
+    state = { events: []}
+
+    componentDidMount() {
+        axios.get(`http://localhost:63342/ScheduleRx/ScheduleRx.API/Bookings/Index.php`)
+      .then(res => {
+        const events = res.data.records;
+        for(let obj of events){
+            obj.START_TIME = moment(obj.START_TIME).toDate();
+            obj.END_TIME = moment(obj.END_TIME).toDate();
+        }
+        this.setState({ events });
+      });
+    };
+    render(){
+        return(
+            <div 
+                className="rbc-calendar"
+            >
+                <Calendar events={this.state.events} />
+            </div>
+        )
+    }
+}
 
 export default Home;
