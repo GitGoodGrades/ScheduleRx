186ace5e2f5e7ac894ceb04010b4b4c341ffc38e
Samantha Thurmon <sthurmon@live.com>
2018-02-25 12:07:08 -0600

    Added events list

--- ScheduleRx.Reactjs/schedulerx/src/index.js
+++ ScheduleRx.Reactjs/schedulerx/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './scenes/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import './app.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 render(
-    (<BrowserRouter className="App">
+    (<MuiThemeProvider >
+        <BrowserRouter className="App">
         <App className="App"/>
-    </BrowserRouter>),
+    </BrowserRouter>
+    </MuiThemeProvider>    ),
     document.getElementById('root')
 );
 
