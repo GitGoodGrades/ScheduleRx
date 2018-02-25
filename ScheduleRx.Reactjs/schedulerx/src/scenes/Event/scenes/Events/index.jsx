6a8cedf0bb09f883f4b2588d811325b8af5599b7
Samantha Thurmon <sthurmon@live.com>
2018-02-25 01:24:16 -0600

    Added events list

---
+++ ScheduleRx.Reactjs/schedulerx/src/scenes/Event/scenes/Events/index.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import EventTable from '../../components/EventTable';
+import CircularProgress from 'material-ui/Progress/CircularProgress';
+
+class Events extends Component {
+    state = {
+        eventList: [],
+        isLoading: true
+    };
+
+    componentDidMount() {
+        axios.get(`http://localhost:63342/ScheduleRx/ScheduleRx.API/Bookings/Index.php`)
+            .then(res => {
+                const eventList = res.data;
+                this.setState({ eventList, isLoading: false});
+            });
+    };
+
+    render(){
+        return(
+            <div>
+                {this.state.isLoading && <CircularProgress size={75} /> ||
+                <EventTable eventList={this.state.eventList} />}
+            </div>
+        );
+    };
+}
+export default Events;
