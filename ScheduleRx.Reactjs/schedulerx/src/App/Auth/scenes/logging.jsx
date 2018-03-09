472b680c62dcdf7215af98b230697d8036fed3fa
stevensavant <stevensavant@gmail.com>
2018-03-09 15:56:03 -0600

    Rebased to master

--- ScheduleRx.Reactjs/schedulerx/src/App/Auth/scenes/logging.jsx
+++ ScheduleRx.Reactjs/schedulerx/src/App/Auth/scenes/logging.jsx
@@ -3,23 +3,30 @@ import { connect } from 'react-redux';
 import {client} from '../../../configuration/client';
 import * as action from '../../../Redux/actions/actionCreator';
 import LoginForm from '../components/login';
+import { Redirect } from 'react-router-dom';
 
 const mapDispatchToProps = (dispatch) => ({
     sendUser: (USER_ID, USER_ROLE, SEMESTER_ID) => dispatch(action.setUser(
-        USER_ID, 
+        USER_ID,
         USER_ROLE,
         SEMESTER_ID
-      ))
-  });
+    ))
+});
 
 class Logging extends React.Component {
     constructor() {
-      super();
+        super();
     }
     state = {
         registrationSchedule: {},
         currentSchedule: {}
-    }
+    };
+
+    saveSession = (myID, myRole, mySem) => {
+        sessionStorage.setItem('myID', myID);
+        sessionStorage.setItem('myRole', myRole);
+        sessionStorage.setItem('mySem', mySem);
+    };
 
     handleSave = (userInfo) => {
         let USER_ID = '';
@@ -28,18 +35,26 @@ class Logging extends React.Component {
         client.post(`Users/Detail.php`, {
             USER_ID: userInfo.USER_ID,
         })
-          .then(res => {
-            if(userInfo.USER_PASSWORD === res.data.USER_PASSWORD){
-                USER_ID = res.data.USER_ID;
-                ROLE_ID = res.data.ROLE_ID;
-                SEMESTER_ID = res.data.SEMESTER_ID;
-            }
-            this.props && this.props.sendUser(USER_ID, ROLE_ID, SEMESTER_ID);
-        }); 
+            .then(res => {
+                if(userInfo.USER_PASSWORD === res.data.USER_PASSWORD){
+                    USER_ID = res.data.USER_ID;
+                    ROLE_ID = res.data.ROLE_ID;
+                    SEMESTER_ID = res.data.SEMESTER_ID;
+                }
+                this.props && this.props.sendUser(USER_ID, ROLE_ID, SEMESTER_ID);
+                this.saveSession(USER_ID, ROLE_ID, SEMESTER_ID);
+            });
     };
 
     render(){
-        
+        const savedUser = sessionStorage.getItem('myID');
+        const savedRole = sessionStorage.getItem('myRole');
+        const savedSem = sessionStorage.getItem('mySem');
+        console.log(savedUser);
+        if (savedUser) {
+            this.props.sendUser(savedUser, savedRole, savedSem);
+            return <Redirect to path="/"/>;
+        }
         return(
             <div>
                 <LoginForm onSave={this.handleSave}/>
