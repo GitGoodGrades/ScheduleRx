dc77c05bd252b6fb0a51de1d1e04bdf56326174e
stevensavant <stevensavant@gmail.com>
2018-03-13 03:46:04 -0500

    Add history object stucture, Add redirect after submmiting New Schedule, Add Alerts for low-level input validation

--- ScheduleRx.Reactjs/schedulerx/src/index.js
+++ ScheduleRx.Reactjs/schedulerx/src/index.js
@@ -6,12 +6,13 @@ import { BrowserRouter } from 'react-router-dom';
 import './App/app.css';
 import { Provider } from 'react-redux';
 import configureStore from './Redux/store/store';
+import history from './App/History';
 
 const store = configureStore();
 
 render((
 <Provider store={store}>
-    <BrowserRouter className="App">
+    <BrowserRouter className="App" history={history}>
         <App style={{fontfamily: 'Cuprum, sans-serif',}} className="App"/>
     </BrowserRouter>
 </Provider>
