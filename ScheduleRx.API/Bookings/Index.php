a632b5df7fd78e7c16a6cd5087f677680defc16f
stevensavant <stevensavant@gmail.com>
2018-03-13 01:14:41 -0500

    Add More info to Event Index

--- ScheduleRx.API/Bookings/Index.php
+++ ScheduleRx.API/Bookings/Index.php
@@ -4,8 +4,17 @@ header("Content-Type: application/json; charset=UTF-8");
 
 include_once '../config/database.php';
 include_once '../SuperCRUD/Index.php';
+include_once  'GetSections.php';
 
 $database = new Database();
 $conn = $database->getConnection();
 
-echo GetAll('booking', 'BOOKING_ID', $conn);
+$results = [];
+$allTheThings = json_decode(GetAll('booking', 'BOOKING_ID', $conn));
+
+foreach ($allTheThings->records as $record) {
+    $record->SECTION = GetSections($record->BOOKING_ID, $conn);
+    array_push($results, $record);
+}
+
+echo json_encode($results);
