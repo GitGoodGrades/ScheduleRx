404ad053b2c610f559c47a84d43250ccc25a399f
WoodNj <Woodnj@warhawks.ulm.edu>
2018-03-13 12:32:30 -0500

    Change history/router pattern

--- ScheduleRx.Reactjs/schedulerx/src/index.js
+++ ScheduleRx.Reactjs/schedulerx/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import './App/index.css';
 import App from './App/App';
-import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import './App/app.css';
 import { Provider } from 'react-redux';
 import configureStore from './Redux/store/store';
@@ -12,8 +12,8 @@ const store = configureStore();
 
 render((
 <Provider store={store}>
-    <BrowserRouter className="App" history={history}>
+    <Router className="App" history={history}>
         <App style={{fontfamily: 'Cuprum, sans-serif',}} className="App"/>
-    </BrowserRouter>
+    </Router>
 </Provider>
 ), document.getElementById('root'));
