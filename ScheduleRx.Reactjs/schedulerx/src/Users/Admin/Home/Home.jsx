5ed65f2094abd556ef33a97e54c9435ed596fc6b
WoodNj <Woodnj@warhawks.ulm.edu>
2018-03-21 15:57:03 -0500

    This is what hell is like

--- ScheduleRx.Reactjs/schedulerx/src/Users/Admin/Home/Home.jsx
+++ ScheduleRx.Reactjs/schedulerx/src/Users/Admin/Home/Home.jsx
@@ -4,7 +4,15 @@ import EventView from '../../../Base Components/eventView';
 import {connect} from 'react-redux';
 import * as action from '../../../Redux/actions/actionCreator';
 import { withStyles } from 'material-ui/styles';
+import HTML5Backend from 'react-dnd-html5-backend';
+import { DragDropContext } from 'react-dnd';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.less'
+BigCalendar.momentLocalizer(moment);
+const DragAndDropCalendar = withDragAndDrop(BigCalendar)
 
 const styles = theme => ({
     root: {
@@ -24,11 +32,6 @@ const styles = theme => ({
     }
 });
 
-
-
-
-
-
 const mapStateToProps = (state) => ({
     events: state.adminCalendar
   });
@@ -38,11 +41,31 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 class EmptyHome extends Component {
-    state = { events: [], event: {}, open: false};
+    constructor(props) {
+        super(props)
+        this.state = {
+            events: [], 
+            event: {}, 
+            open: false
+        }
+    
+        this.moveEvent = this.moveEvent.bind(this)
+    }
+    // state = {
+    //     events: [], 
+    //     event: {}, 
+    //     open: false
+    // }
+
+    componentWillReceiveProps = (nextProps) => {
+        this.setState({events: nextProps.events})
+    }
+    
 
     componentDidMount() {
         this.props.onLoad();
     };
+
     handleSelectSlot = () => {
         console.log();
     }
@@ -54,18 +77,44 @@ class EmptyHome extends Component {
     handleClose = () => {
         this.setState({ open: false})
     }
+    
+
+    moveEvent = ({ event, START_TIME, END_TIME }) => {
+        const  events  = this.state.events
+    
+        const idx = events.indexOf(event)
+        const updatedEvent = { ...event, START_TIME, END_TIME }
+    
+        const nextEvents = [...events]
+        nextEvents.splice(idx, 1, updatedEvent)
+    
+        this.setState({
+          events: nextEvents,
+        })
+    
+        alert(`${event.BOOKING_TITLE} was dropped onto ${event.START_TIME}`)
+    }
+
+
     render() {
         const {classes} = this.props;
         return (
             <div
                 className={classes.root}
+                
             >
                 <div className={classes.container}>
-                <Calendar
-                    className={classes.cal}
-                    events={this.props.events}
-                    handleEventSelection={this.handleSelectEvent}
-                    handleSlotSelection={this.handleSelectSlot}
+                <DragAndDropCalendar
+                    style={{height: 500}}
+                    selectable
+                    events={this.state.events}
+                    onEventDrop={this.moveEvent}
+                    titleAccessor="BOOKING_TITLE"
+                    startAccessor='START_TIME'
+                    endAccessor='END_TIME'
+                    onSelectEvent={event => this.handleSelectEvent(event)}
+                    defaultDate={new Date()}
+                    onSelectSlot={slotInfo => this.handleSelectSlot(slotInfo)}
                     views={['month', 'week', 'day']}
                 />
                 <EventView event={this.state.event} open={this.state.open} onClose={this.handleClose} />
@@ -74,6 +123,6 @@ class EmptyHome extends Component {
         )
     }
 }
-const Home =  withStyles(styles, {withTheme: true})(EmptyHome)
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+const StyleHome =  withStyles(styles, {withTheme: true})(EmptyHome)
+const Home = connect(mapStateToProps, mapDispatchToProps)(StyleHome)
+export default DragDropContext(HTML5Backend)(Home);
