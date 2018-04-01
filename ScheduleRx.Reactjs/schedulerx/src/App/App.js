4184891cb288635d9161e5c8889cfb29cea15c99
WoodNj <Woodnj@warhawks.ulm.edu>
2018-04-01 18:10:37 -0500

    Finish the full edit view form

--- ScheduleRx.Reactjs/schedulerx/src/App/App.js
+++ ScheduleRx.Reactjs/schedulerx/src/App/App.js
@@ -5,15 +5,22 @@ import Shell from './Shell/Shell';
 import { ulmTheme as theme } from '../MaterialUI/theme/index';
 import Logging from './Auth/scenes/logging';
 import Registration from './Auth/scenes/registration';
+import MomentUtils from 'material-ui-pickers/utils/moment-utils';
+import MuiPickersUtilsProvider from 'material-ui-pickers/utils/MuiPickersUtilsProvider';
+
+
 
 const App = () =>
     (
         <MuiThemeProvider theme={theme}>
-            <Switch>
-                <Route exact path="/login" component={Logging}/>
-                <Route exact path="/Register" component={Registration}/>
-                <Route path="/" component={Shell}/>
-            </Switch>
+            <MuiPickersUtilsProvider utils={MomentUtils}>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+                <Switch>
+                    <Route exact path="/login" component={Logging}/>
+                    <Route exact path="/Register" component={Registration}/>
+                    <Route path="/" component={Shell}/>
+                </Switch>
+            </MuiPickersUtilsProvider>    
         </MuiThemeProvider>
     );
 
