96c5628263ad14d0debaad5d29f440f26cb4ba4d
WoodNj <Woodnj@warhawks.ulm.edu>
2018-04-02 16:49:19 -0500

    mainline it

--- ScheduleRx.Reactjs/schedulerx/src/Users/Lead/Home/Home.jsx
+++ ScheduleRx.Reactjs/schedulerx/src/Users/Lead/Home/Home.jsx
@@ -4,6 +4,8 @@ import {connect} from 'react-redux';
 import * as action from '../../../Redux/actions/actionCreator';
 import EventView from '../../../Base Components/eventView';
 import { withStyles } from 'material-ui/styles';
+import moment from 'moment';
+import { client } from '../../../configuration/client';
 
 
 const styles = theme => ({
@@ -30,11 +32,22 @@ const mapStateToProps = (state) => ({
     user: state.userName,
     semester: state.semester,
     schedule: state.currentSchedule,
-    calendar: state.userCalendar
+    events: state.userCalendar,
+    courses: state.courseList,
+    rooms: state.roomList,
+    sections: state.sectionList,
+    current_schedule: state.currentSchedule,
+    registration_schedule: state.registrationSchedule,
+    conflict_List: state.conflictList,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    onLoad: (user, role) => dispatch(action.userCalendar(user, role))
+    onLoad: (user, role) => dispatch(action.userCalendar(user, role)),
+    loadCourses: () => dispatch(action.searchCourses()),
+    loadRooms: () => dispatch(action.searchRooms()),
+    loadSections: () => dispatch(action.searchSections()),
+    loadSchedules: () => dispatch(action.searchSchedules()),
+    getConflictEvents: () => dispatch(action.searchConflicts())
 });
 
 class EmptyHome extends Component {
@@ -44,12 +57,16 @@ class EmptyHome extends Component {
         open: false
     };
 
-    componentDidMount() {
-        this.props.onLoad(this.props.user, this.props.role)
+    componentWillReceiveProps = (nextProps) => {
+        this.setState({events: nextProps.events})
     }
 
-    componentWillReceiveProps = (nextProps) => {
-        this.setState({events: nextProps.calendar})
+    componentDidMount() {
+        this.props.onLoad(this.props.user, this.props.role),
+        this.props.loadCourses(),
+        this.props.loadRooms(),
+        this.props.loadSections(),
+        this.props.loadSchedules();
     }
 
     handleSelectSlot = () => {
@@ -63,6 +80,67 @@ class EmptyHome extends Component {
     handleClose = () => {
         this.setState({ open: false})
     }
+
+    handleSave = (event) => {        
+        this.save(event);
+    }
+
+    save = (event) => {
+        let scheduleID = null;
+        this.props.getConflictEvents(event.START_TIME, event.END_TIME);
+        let conflicts = this.props.conflict_List;
+
+        client.post(`Bookings/Delete.php`, {
+            BOOKING_ID: event.BOOKING_ID
+        });
+
+        let temp = this.state.events;
+        temp.map(old => {
+            if(old.BOOKING_ID === event.BOOKING_ID){
+                temp.splice(temp.indexOf(old), 1);
+            }
+        })
+
+        if(conflicts == null){
+            if(moment(event.START_TIME).isBetween(
+                this.props.current_schedule.START_SEM_DATE,
+                this.props.current_schedule.END_SEM_DATE)){
+
+                scheduleID = null;
+                // CREATE REQUEST LOGIC HERE
+
+            } else if (moment(event.START_TIME).isBetween(
+                this.props.registration_schedule.START_SEM_DATE,
+                this.props.registration_schedule.END_SEM_DATE)){
+                    scheduleID = this.props.registration_schedule.SCHEDULE_ID;
+            }
+        } else {
+            //conflict logic
+        }
+
+        client.post(`Bookings/Create.php`, {
+            SCHEDULE_ID: scheduleID,
+            COURSE_ID: event.course,
+            SECTION_ID: event.sections,
+            ROOM_ID: event.room,
+            START_TIME: event.START_TIME,
+            END_TIME: event.END_TIME,
+            BOOKING_TITLE: event.title,
+            NOTES: event.details
+        })
+            .then(res => {
+                temp.push(res.data);
+                this.setState({events: temp})
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        this.setState({
+          open: false
+        });
+    };
+
     render() {
         const {classes} = this.props;
         return (
@@ -75,13 +153,21 @@ class EmptyHome extends Component {
                     events={this.state.events}
                     handleEventSelection={this.handleSelectEvent}
                     handleSlotSelection={this.handleSelectSlot}
+                    views={['month', 'week', 'day']}
                 />
-                <EventView event={this.state.event} open={this.state.open} onClose={this.handleClose} />
+                <EventView 
+                    event={this.state.event} 
+                    open={this.state.open} 
+                    onClose={this.handleClose} 
+                    courseList={this.props.courses} 
+                    sectionList={this.props.sections} 
+                    roomList={this.props.rooms} 
+                    onSave={this.handleSave}/>
             </div>
         </div>
         )
     }
 }
 
-const Home = withStyles(styles)(EmptyHome);
+const Home = withStyles(styles, {withTheme: true})(EmptyHome);
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
