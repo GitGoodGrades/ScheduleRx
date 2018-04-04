966d30c2b2578ebb53bd1c07dc84ebd9b9976e7c
stevensavant <stevensavant@gmail.com>
2018-04-04 16:42:32 -0500

    Create Rooms with Capabiliites, Update Rooms and Capabilities

--- ScheduleRx.API/Room/Create.php
+++ ScheduleRx.API/Room/Create.php
@@ -14,6 +14,25 @@ $conn = $database->getConnection();
 $data = json_decode(file_get_contents("php://input"));
 $log = Logger::getLogger("RoomLog");
 
+
+/*  Script - Creates a Room and it's associations to the capabilities table in the database
+ *  @param ROOM_ID |        The room ID (Room Number)
+ *  @param CAPACITY |       The capacity of the room
+ *  @param ROOM_NAME |      The name of the room (if applicable)
+ *  @param LOCATION |       The building the room is located in
+ *  @param DESCRIPTION |    A description of the room
+ *  @param CAPABILITIES |   A list of int CAPABILITY ID's that this room has
+ *  @return                 The response if the room was successfully created
+ */
+$capabilities = $data->CAPABILITIES;
+unset($data->CAPABILITIES);
+
 $response = CreateRecord('room', $data, $conn);
 $log->info($response);
+
+foreach ($capabilities as $caps) {
+    $newAssoc = array( "ROOM_ID" => $data->ROOM_ID, "CAPABILITY_ID" =>$caps);
+    $log->info(CreateRecord('room_capabilities', $newAssoc, $conn));
+}
+
 echo $response;
