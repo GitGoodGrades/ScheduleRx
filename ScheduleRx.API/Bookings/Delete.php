6d8635e7fd7795a58e438cc92824fde20421aa3f
stevensavant <stevensavant@gmail.com>
2018-04-05 19:26:15 -0500

    Add Event/Conflict Relational Delete Logic

--- ScheduleRx.API/Bookings/Delete.php
+++ ScheduleRx.API/Bookings/Delete.php
@@ -6,22 +6,41 @@ header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once '../config/database.php';
-include  '../SuperCRUD/Delete.php';
+include_once  '../SuperCRUD/Delete.php';
+include_once  '../SuperCRUD/Search.php';
 include_once '../config/LogHandler.php';
 
 $database = new Database();
 $conn = $database->getConnection();
 $data = json_decode(file_get_contents("php://input"));
-$log = Logger::getLogger('EventLog');
+$log = Logger::getLogger('EventAssociationsLog');
 
 /* Script
  * Deletes and event from the 'booking' table and it's associations from the event_sections if  any exist
  */
-$eventResponse =  DeleteRecord('booking',"BOOKING_ID", $data->BOOKING_ID, $conn );
-$sectionResponse = DeleteRecord('event_section', 'BOOKING_ID', $data->BOOKING_ID, $conn);
+$recordsOfConflict = json_decode(Search('conflict_event', "BOOKING_ID" , "'" . $data->BOOKING_ID . "'" ,$conn));
 
-echo $eventResponse;
-echo $sectionResponse;
+if($data->SCHEDULE_ID == null || $data->SCHEDULE_ID == "") {
+    foreach ($recordsOfConflict->records as $cRecord) {
+        DeleteRecord('conflict_event',"CONFLICT_ID", $cRecord->CONFLICT_ID, $conn );
+        DeleteRecord('conflict',"CONFLICT_ID", $cRecord->CONFLICT_ID, $conn );
+    }
+}
+else {
+    if ($recordsOfConflict != null) {
+        foreach ($recordsOfConflict->records as $cRecord) {
+            DeleteRecord('conflict_event',"BOOKING_ID", $data->BOOKING_ID . " AND CONFLICT_ID=" . $temp->CONFLICT_ID, $conn );
+            $temp = json_decode(Search('conflict_event', "CONFLICT_ID" , "'" . $cRecord->CONFLICT_ID . "'" ,$conn));
+
+            if(count($temp->records) < 2) {
+                DeleteRecord('conflict_event',"CONFLICT_ID", $cRecord->CONFLICT_ID, $conn );
+                DeleteRecord('conflict',"CONFLICT_ID", $cRecord->CONFLICT_ID, $conn );
+            }
+        }
+    }
+}
+$eventResponse =  DeleteRecord('booking',"BOOKING_ID", "'" . $data->BOOKING_ID . "'", $conn );
+$sectionResponse = DeleteRecord('event_section', 'BOOKING_ID', $data->BOOKING_ID, $conn);
 
 if ($eventResponse != null) {
     $log->info($eventResponse);
--- ScheduleRx.API/SuperCRUD/Delete.php
+++ ScheduleRx.API/SuperCRUD/Delete.php
@@ -1,10 +1,18 @@
 <?php
+include_once '../config/LogHandler.php';
 function DeleteRecord ($tableName, $PrimaryKey, $value, $conn) {
+    $log = Logger::getLogger('EventDeleteLog');
     global $stmt;
     $query = ('DELETE FROM ' . $tableName ." WHERE " . $PrimaryKey . '=:' . $PrimaryKey);
     $stmt = $conn->prepare($query);
     $stmt->bindValue(":". $PrimaryKey, $value);
 
-    return $stmt->execute() ? $tableName . ' was deleted.' : null; //$tableName . ' was not deleted ERROR CODE:' . $stmt->errorCode();
+    if ($stmt->execute()) {
+        return  $tableName . ' was deleted.';
+    }
+    else {
+        $log->warn ($tableName . ' was not deleted ERROR CODE:' . $stmt->errorCode());
+        return null;
+    }
 }
 
