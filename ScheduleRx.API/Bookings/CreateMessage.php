3f5f2bb694823ab4473c78f7efdba62b83c731fa
stevensavant <stevensavant@gmail.com>
2018-04-10 09:24:09 -0500

    gamma

--- ScheduleRx.API/Bookings/CreateMessage.php
+++ ScheduleRx.API/Bookings/CreateMessage.php
@@ -1,5 +1,6 @@
 <?php
 include_once '../models/getGUID.php';
+include_once '../SuperCRUD/Create.php';
 
 function CreateMessage($mssage , $userID, $conn) {
     $newMessage = array("USER_ID" => $userID, "MESSAGE" => $mssage, "MSG_ID" => substr((string)getGUID(), 1, 36));
