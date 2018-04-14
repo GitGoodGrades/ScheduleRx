eea797dea1ffbfe34db0355f7413ea04f663ff03
stevensavant <stevensavant@gmail.com>
2018-04-13 19:11:43 -0500

    Append First and Last Name to users Details

--- ScheduleRx.API/Users/Detail.php
+++ ScheduleRx.API/Users/Detail.php
@@ -6,11 +6,21 @@ header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
 include_once '../config/database.php';
+include_once '../config/Banner_DB.php';
 include_once '../SuperCRUD/Detail.php';
 
 
 $database = new Database();
+$banner = new Banner_DB();
 $conn = $database->getConnection();
+$con = $banner->getConnection();
 $data = json_decode(file_get_contents("php://input"));
 
-echo FindRecord('users', 'USER_ID', $data->USER_ID, $conn);
+$RxInfo = json_decode(FindRecord('users', 'USER_ID', $data->USER_ID, $con));
+$bannerInfo = json_decode(FindRecord('users', 'USER_ID', $data->USER_ID, $con));
+
+
+$RxInfo->FIRSTNAME = $bannerInfo->FIRSTNAME;
+$RxInfo->LASTNAME = $bannerInfo->LASTNAME;
+
+echo json_encode($RxInfo);
