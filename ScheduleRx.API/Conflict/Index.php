e76399de269047bd476bb811e1fb90abaea0843e
stevensavant <stevensavant@gmail.com>
2018-04-14 21:05:32 -0500

    Additional Bad Gateway Patches

--- ScheduleRx.API/Conflict/Index.php
+++ ScheduleRx.API/Conflict/Index.php
@@ -1,8 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Content-Type: application/json; charset=UTF-8");
 
 include_once '../config/database.php';
 include_once '../SuperCRUD/Search.php';
