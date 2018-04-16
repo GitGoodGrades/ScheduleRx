7bf678b3f81cdef131f8134f117eece556b2d993
stevensavant <stevensavant@gmail.com>
2018-04-16 00:02:22 -0500

    Messaging with Defaults - Approve

--- ScheduleRx.API/Bookings/ApproveDelete.php
+++ ScheduleRx.API/Bookings/ApproveDelete.php
@@ -14,30 +14,12 @@ include_once '../SuperCRUD/Detail.php';
 include_once '../SuperCRUD/Index.php';
 include_once 'CreateMessage.php';
 include_once '../SuperCRUD/Create.php';
-//include_once '../Schedule/findByTime.php';
+include_once '../Schedule/findByTime.php';
 
 $database = new Database();
 $conn = $database->getConnection();
 $data = json_decode(file_get_contents("php://input"));
 $log = Logger::getLogger('ApproveLog');
-
-function findByTime( $start, $conn, $logger) {
-    $allSchedules = json_decode(GetAll('schedule', 'SCHEDULE_ID', $conn));
-    if (!$allSchedules) {
-        return null;
-    }
-    $logger->info("given start format: " . $start);
-    //return 'obtained urainium';
-    foreach ($allSchedules->records as $record ) {
-        if (($record->START_SEM_DATE <= $start) && ($start >= $record->END_SEM_DATE)) {
-            $logger->info("matching time frame found: " . $start);
-            return $record->SCHEDULE_ID;
-        }
-    }
-
-    $logger->info("no matching time found");
-    return null;          //json_encode(array("message" => "No Conflicts found."));
-}
 
 /* Script
  * Deletes multiple event by ID on approval and updates the requested SCHEDULE_ID of the approved event
@@ -46,14 +28,8 @@ function findByTime( $start, $conn, $logger) {
  * @param CONFLICT_ID | The conflict ID for the approval
  * @param MESSAGE | The to Send to the Deleted Event Leads
  */
-
-//if (!isset($data->EVENTS, $data->APPROVED, $data->CONFLICT_ID)) {
-//   $log->warn("Improper Parameters Supplied");
-//   exit(null);
-//}
-
 $data->APPROVED->SCHEDULE_ID = findByTime($data->APPROVED->START_TIME, $conn, $log);
-$log->info('SCHEDULE_ID changed to: ' . $data->APPROVED->SCHEDULE_ID);
+$approvedCourse =  $data->APPROVED->SECTIONS->records[0]->COURSE_ID;
 
 DeleteRecord('conflict_event',"CONFLICT_ID", $data->CONFLICT_ID, $conn );
 DeleteRecord('conflict',"CONFLICT_ID", $data->CONFLICT_ID, $conn );
@@ -65,18 +41,21 @@ foreach ($data->EVENTS as $event) {
     $SectionInfo = null;
     $leadID = null;
     $log->info("Has a Section of COURSE: " . $eventSection['records'][0]['COURSE_ID']);
+
     if ($eventSection && isset($data->MESSAGE)) {
         $log->info("Searching for Lead of This Course");
         $leadID = json_decode(FindRecord('leads_course', 'COURSE_ID', $eventSection['records'][0]['COURSE_ID'], $conn));
 
-        $log->info("Lead of Course FoundXXXXXXX: " . $leadID->USER_ID);
+        $log->info("Lead of Course Found: " . $leadID->USER_ID);
         CreateMessage($data->MESSAGE, $leadID->USER_ID, $conn);
     }
 
     DeleteRecord('event_section', 'BOOKING_ID',   $event->BOOKING_ID , $conn);
     DeleteRecord('booking',"BOOKING_ID",   $event->BOOKING_ID , $conn );
 }
-
 unset($data->APPROVED->SECTIONS);
 UpdateRecord('booking', $data->APPROVED ,"BOOKING_ID", $conn);
 echo GetAll('conflict', "CONFLICT_ID", $conn);
+
+$leadID = json_decode(FindRecord('leads_course', 'COURSE_ID', $approvedCourse, $conn));
+if ($leadID) { CreateMessage("Your Event Has Been Approved", $leadID->USER_ID, $conn); }
--- ScheduleRx.API/Schedule/findByTime.php
+++ ScheduleRx.API/Schedule/findByTime.php
@@ -1,15 +1,19 @@
 <?php
 
-function findByTime( $start, $conn) {
+function findByTime( $start, $conn, $logger) {
     $allSchedules = json_decode(GetAll('schedule', 'SCHEDULE_ID', $conn));
     if (!$allSchedules) {
         return null;
     }
-    //return 'obtained urainium';
+    $logger->info("given start format: " . $start);
+
     foreach ($allSchedules->records as $record ) {
         if (($record->START_SEM_DATE <= $start) && ($start >= $record->END_SEM_DATE)) {
+            $logger->info("matching time frame found: " . $start);
             return $record->SCHEDULE_ID;
         }
     }
+
+    $logger->info("no matching time found");
     return null;          //json_encode(array("message" => "No Conflicts found."));
 }
