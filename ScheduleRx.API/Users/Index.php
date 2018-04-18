f251cfe109c5a0731a67c8b5dc2b002df969e35f
stevensavant <stevensavant@gmail.com>
2018-04-18 18:31:01 -0500

    Deployment Changes

--- ScheduleRx.API/Users/Index.php
+++ ScheduleRx.API/Users/Index.php
@@ -1,8 +1,9 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
 header("Content-Type: application/json; charset=UTF-8");
 
-include_once $_SERVER['DOCUMENT_ROOT'] . "/ScheduleRx.API/rxapi.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/ScheduleRx.API/rxapi.php";
 
 
 $database = new Database();
