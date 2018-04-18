990d6360714b6aa50afc1795bebcacd82bb68c72
stevensavant <stevensavant@gmail.com>
2018-04-18 18:51:36 -0500

    Sync branch with master

--- ScheduleRx.API/Bookings/Update.php
+++ ScheduleRx.API/Bookings/Update.php
@@ -4,14 +4,7 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-<<<<<<< HEAD
 include_once $_SERVER["DOCUMENT_ROOT"] . "/ScheduleRx.API/rxapi.php";
-=======
-include_once '../config/database.php';
-include_once  '../SuperCRUD/Update.php';
-include_once  '../SuperCRUD/Delete.php';
-include_once  '../SuperCRUD/Create.php';
->>>>>>> master
 
 $database = new Database();
 $conn = $database->getConnection();
