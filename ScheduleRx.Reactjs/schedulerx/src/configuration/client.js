c045a8df2ba816c3db197407733f31abc19f2b3a
Samantha Thurmon <sthurmon@live.com>
2018-04-25 10:41:07 -0500

    Change Client

--- ScheduleRx.Reactjs/schedulerx/src/configuration/client.js
+++ ScheduleRx.Reactjs/schedulerx/src/configuration/client.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const client = axios.create({
-    baseURL: 'http://localhost:63342/ScheduleRx/ScheduleRx.API/'
+    baseURL: 'http://localhost:63342/ScheduleRx.API/'
 })
